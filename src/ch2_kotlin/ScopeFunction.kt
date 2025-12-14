2f5340970d4aafd0f16b64cc2bc51e509f2eae01
Suhyeon7497 <nucc1186@gmail.com>
2025-12-14 16:53:03 +0900

    ch2_kotlin scope function

---
+++ src/ch2_kotlin/ScopeFunction.kt
@@ -0,0 +1,30 @@
+package ch2_kotlin
+
+class User(var name: String = "", var age: Int = 0)
+
+fun main(args: Array<String>) {
+    var user = User("Dmitry")
+    val nameLength = user?.let {
+        it.name.length
+    }
+    println(nameLength)
+
+    user = User().apply {
+        name = "apply"
+        age = 0
+    }
+
+    println("${user.name} is ${user.age}")
+    user
+        .also { it.name = "also" }
+        .also { println("${it.name} is ${it.age}") }
+
+    val result = user.run {
+        age + name.length
+    }
+    println(result)
+
+    with(user) {
+        println("$name, $age")
+    }
+}
